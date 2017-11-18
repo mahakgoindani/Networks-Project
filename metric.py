e9bdb01af8f3bb8285e976ee5c01dd22ecc8cba8
Animesh Sinha <sinha.animesh34@gmail.com>
2017-11-18 15:26:16 +0530

    Changes

--- metric.py
+++ metric.py
@@ -6,6 +6,6 @@ def get_data():
     training_labels = genfromtxt('../data/training_labels.txt', delimiter=',', usecols = range(1) , dtype=None)
     validation_samples = genfromtxt('../data/validation_set.csv', delimiter=',', usecols = range(41))
     validation_labels = genfromtxt('../data/validation_labels.txt', delimiter=',', usecols = range(1) , dtype=None)
-    test_samples = genfromtxt('test_set.csv', delimiter=',', usecols = range(41))
+    test_samples = genfromtxt('../data/test_set.csv', delimiter=',', usecols = range(41))
     return training_samples, validation_samples, test_samples, training_labels, validation_labels
 
--- svm.py
+++ svm.py
@@ -1,11 +1,14 @@
-from metrics import get_data
-from sklearn.svm import SVC
+from metric import get_data
+from sklearn import svm
+from sklearn.svm import LinearSVC
 from sklearn.metrics import precision_recall_fscore_support
 
 if __name__ == "__main__":
   training_samples, validation_samples, test_samples, training_labels, validation_labels = get_data()
-  
-  clf = SVC()
-  clf.fit(training_samples, training_labels);
-  predictions = clf.predict(validation_samples);
-  print precision_recall_fscore_support(validation_labels, predictions)
+  CArray=[0.1,1,10,100,1000]
+  for C in CArray:
+    print "C: " + str(C)
+    clf = svm.LinearSVC(C=C)
+    clf.fit(training_samples, training_labels)
+    predictions = clf.predict(validation_samples)
+    print precision_recall_fscore_support(validation_labels, predictions)
